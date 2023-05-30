3ee38288f2a0011e6120b9e84d8ed10c0dbd9e9e
xuanchengwei <xuanchengwei@coros.com>
2023-05-30 14:07:06 +0800

    Update BaicizhanParseApplicationTests.java

--- src/test/java/com/example/baicizhanparse/BaicizhanParseApplicationTests.java
+++ src/test/java/com/example/baicizhanparse/BaicizhanParseApplicationTests.java
@@ -1,10 +1,19 @@
 package com.example.baicizhanparse;
 
 import com.example.baicizhanparse.dao.DictDao;
+import com.example.baicizhanparse.entity.Roadmap;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.io.Files;
 import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
 
 @SpringBootTest
 class BaicizhanParseApplicationTests {
@@ -12,10 +21,22 @@ class BaicizhanParseApplicationTests {
     @Autowired
     DictDao dictDao;
 
+    File jsonFile = new File("metadata/road_map_621.baicizhan.json");
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException {
         System.out.println(dictDao.selectCount(null));
+        String json = Files.asCharSource(jsonFile, Charset.defaultCharset()).read();
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Roadmap> roadmapList = objectMapper.readValue(json, new TypeReference<>() {});
+
+        for (Roadmap roadmap : roadmapList) {
+            System.out.println(roadmap.getTopicId());
+        }
+
+
+
+
     }
 
 }
