5978b8cf4a7db02894b21e02ab9d6ea9ea43175b
xuanchengwei <xuanchengwei@coros.com>
2023-05-30 14:01:40 +0800

    cd

---
+++ src/main/java/com/example/baicizhanparse/dao/DictDao.java
@@ -0,0 +1,16 @@
+package com.example.baicizhanparse.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.baicizhanparse.entity.Dict;
+
+/**
+ * (Dict)表数据库访问层
+ *
+ * @author xuanchengwei
+ * @since 2023-05-30 14:00:25
+ */
+public interface DictDao extends BaseMapper<Dict> {
+
+
+}
+
---
+++ src/main/java/com/example/baicizhanparse/entity/Dict.java
@@ -0,0 +1,29 @@
+package com.example.baicizhanparse.entity;
+
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * (Dict)表实体类
+ *
+ * @author xuanchengwei
+ * @since 2023-05-30 14:00:25
+ */
+@Data
+public class Dict {
+
+    private Integer topicId;
+
+    private String word;
+
+    private String accent;
+
+    private String meanCn;
+
+    private String freq;
+
+    private Integer wordLength;
+}
+
