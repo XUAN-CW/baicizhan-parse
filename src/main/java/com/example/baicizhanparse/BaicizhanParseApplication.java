ad1388c7ddef50b96a0019c75e9ba46ad7ff768f
xuanchengwei <xuanchengwei@coros.com>
2023-05-30 13:37:04 +0800

    创建项目

---
+++ src/main/java/com/example/baicizhanparse/BaicizhanParseApplication.java
@@ -0,0 +1,13 @@
+package com.example.baicizhanparse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaicizhanParseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaicizhanParseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/baicizhanparse/BaicizhanParseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baicizhanparse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaicizhanParseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
