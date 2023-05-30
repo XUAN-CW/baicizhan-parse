643ce3637ba17e3168a11a562388a56015888e9a
xuanchengwei <xuanchengwei@coros.com>
2023-05-30 19:43:38 +0800

    cd

---
+++ src/test/java/com/example/baicizhanparse/Rule34Test.java
@@ -0,0 +1,39 @@
+package com.example.baicizhanparse;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Rule34Test {
+    public static void main(String[] args) throws IOException {
+        File[] rule34HtmlList = new File("metadata/Rule34").listFiles();
+        for (File rule34Html : rule34HtmlList) {
+            parseHtml(rule34Html);
+            break;
+        }
+    }
+
+
+    public static void parseHtml(File rule34Html) throws IOException {
+        Document doc = Jsoup.parse(rule34Html, "UTF-8");
+        // Use XPath expression to retrieve elements
+        String xpathExpression = "/html/body/div[1]/table/tbody/tr/";
+        Elements elements = doc.select(xpathExpression);
+
+        // Check if any matching elements are found
+        if (!elements.isEmpty()) {
+            // Process each element
+            for (Element element : elements) {
+                // Do something with the element
+                System.out.println("XPath result: " + element.html());
+            }
+        } else {
+            System.out.println("No elements found with XPath expression: " + xpathExpression);
+        }
+    }
+
+}
