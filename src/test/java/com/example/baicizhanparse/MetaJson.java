1be50d5ae96be6abd5a553f3db8e054e87922b40
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2023-07-28 07:50:10 +0800

    Update MetaJson.java

--- src/test/java/com/example/baicizhanparse/MetaJson.java
+++ src/test/java/com/example/baicizhanparse/MetaJson.java
@@ -6,58 +6,20 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class MetaJson {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File inputFile = new File("./testData/allegation_zp_117_621_0_20230712135847/zp_117_621_0_20230712135847.zpk"); // Replace with the actual path to your input file
-        File outputFile = new File("output.txt"); // Output JPG file
+        File outputFile = new File("meta.json"); // Output JPG file
 
-        try {
-            byte[] fileData = Files.readAllBytes(inputFile.toPath());
+        byte[] fileData = Files.readAllBytes(inputFile.toPath());
+        byte[] jpgData = new byte[895];
+        System.arraycopy(fileData, 128, jpgData, 0, jpgData.length);
 
-            int startMarkerIndex = indexOf(fileData, new byte[]{(byte) 0xFF, (byte) 0xD8, (byte) 0xFF});
-            int endMarkerIndex = lastIndexOf(fileData, new byte[]{(byte) 0xFF, (byte) 0xD9});
+        FileOutputStream fos = new FileOutputStream(outputFile);
+        fos.write(jpgData);
+        fos.close();
 
-            if (startMarkerIndex != -1 && endMarkerIndex != -1 && startMarkerIndex < endMarkerIndex) {
-                byte[] jpgData = new byte[895];
-                System.arraycopy(fileData, 128, jpgData, 0, jpgData.length);
+        System.out.println("JPEG data extracted and saved as output.jpg.");
 
-                FileOutputStream fos = new FileOutputStream(outputFile);
-                fos.write(jpgData);
-                fos.close();
-
-                System.out.println("JPEG data extracted and saved as output.jpg.");
-            } else {
-                System.out.println("JPEG data not found in the file.");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
-    // Custom implementation of indexOf for byte arrays
-    private static int indexOf(byte[] source, byte[] pattern) {
-        outer:
-        for (int i = 0; i <= source.length - pattern.length; i++) {
-            for (int j = 0; j < pattern.length; j++) {
-                if (source[i + j] != pattern[j]) {
-                    continue outer;
-                }
-            }
-            return i;
-        }
-        return -1;
-    }
-
-    // Custom implementation of lastIndexOf for byte arrays
-    private static int lastIndexOf(byte[] source, byte[] pattern) {
-        outer:
-        for (int i = source.length - pattern.length; i >= 0; i--) {
-            for (int j = 0; j < pattern.length; j++) {
-                if (source[i + j] != pattern[j]) {
-                    continue outer;
-                }
-            }
-            return i;
-        }
-        return -1;
-    }
 }
