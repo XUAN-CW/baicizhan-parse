952540818e1edd0e8409d7817e34b7c938a36458
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2023-07-28 07:23:32 +0800

    Update MP3Extractor.java

--- src/test/java/com/example/baicizhanparse/MP3Extractor.java
+++ src/test/java/com/example/baicizhanparse/MP3Extractor.java
@@ -4,35 +4,46 @@ import com.google.common.io.Files;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Path;
 
 public class MP3Extractor {
     public static void main(String[] args) {
-        File inputFile = new File("./testData/allegation_zp_117_621_0_20230712135847/zp_117_621_0_20230712135847.zpk");
-        File outputFile = new File("output.mp3");
-        extractMP3AudioData(inputFile, outputFile);
+        File inputFile = new File("./testData/allegation_zp_117_621_0_20230712135847/zp_117_621_0_20230712135847.zpk"); // Replace with the actual path to your input file
+        extractAllMP3AudioData(inputFile);
     }
 
-    private static void extractMP3AudioData(File inputFile, File outputFile) {
+    private static void extractAllMP3AudioData(File inputFile) {
         try {
             byte[] data = Files.toByteArray(inputFile);
-            int mp3StartIndex = findMP3StartIndex(data);
+            int currentIndex = 0;
+            int frameCount = 0;
 
-            if (mp3StartIndex != -1) {
-                byte[] mp3AudioData = new byte[data.length - mp3StartIndex];
-                System.arraycopy(data, mp3StartIndex, mp3AudioData, 0, mp3AudioData.length);
-                saveMP3ToFile(mp3AudioData, outputFile);
-            } else {
-                System.out.println("MP3 audio data not found in the file.");
+            while (currentIndex < data.length) {
+                int mp3StartIndex = findMP3StartIndex(data, currentIndex);
+
+                if (mp3StartIndex != -1) {
+                    byte[] mp3AudioData = new byte[data.length - mp3StartIndex];
+                    System.arraycopy(data, mp3StartIndex, mp3AudioData, 0, mp3AudioData.length);
+
+                    // Save the MP3 audio data to a new .mp3 file
+                    File outputFile = new File("output_" + frameCount + ".mp3");
+                    saveMP3ToFile(mp3AudioData, outputFile);
+
+                    currentIndex = mp3StartIndex + 1; // Move the current index to continue searching for the next frame.
+                    frameCount++;
+                } else {
+                    break; // No more MP3 frames found.
+                }
             }
+
+            System.out.println("Extracted " + frameCount + " MP3 audio frames.");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static int findMP3StartIndex(byte[] data) {
-        // Search for the magic number "ID3" or "49 44 33" in the binary data.
-        for (int i = 0; i < data.length - 2; i++) {
+    private static int findMP3StartIndex(byte[] data, int startIndex) {
+        // Search for the magic number "ID3" or "49 44 33" in the binary data, starting from the given index.
+        for (int i = startIndex; i < data.length - 2; i++) {
             if (data[i] == 0x49 && data[i + 1] == 0x44 && data[i + 2] == 0x33) {
                 return i;
             }
