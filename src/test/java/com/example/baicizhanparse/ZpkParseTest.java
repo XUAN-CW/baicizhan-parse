5446bf0cc93605032ccb82847e5dcca01f2e3e9f
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2023-07-29 01:12:59 +0800

    Update ZpkParseTest.java

--- src/test/java/com/example/baicizhanparse/ZpkParseTest.java
+++ src/test/java/com/example/baicizhanparse/ZpkParseTest.java
@@ -64,4 +64,56 @@ public class ZpkParseTest {
 
     }
 
+    public static void getJpg(File inputFile,File outputFile) {
+
+        try {
+            byte[] fileData = java.nio.file.Files.readAllBytes(inputFile.toPath());
+
+            int startMarkerIndex = indexOf(fileData, new byte[]{(byte) 0xFF, (byte) 0xD8, (byte) 0xFF});
+            int endMarkerIndex = lastIndexOf(fileData, new byte[]{(byte) 0xFF, (byte) 0xD9});
+
+            if (startMarkerIndex != -1 && endMarkerIndex != -1 && startMarkerIndex < endMarkerIndex) {
+                byte[] jpgData = new byte[endMarkerIndex - startMarkerIndex + 2];
+                System.arraycopy(fileData, startMarkerIndex, jpgData, 0, jpgData.length);
+
+                FileOutputStream fos = new FileOutputStream(outputFile);
+                fos.write(jpgData);
+                fos.close();
+
+                System.out.println("JPEG data extracted and saved as output.jpg.");
+            } else {
+                System.out.println("JPEG data not found in the file.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Custom implementation of indexOf for byte arrays
+    private static int indexOf(byte[] source, byte[] pattern) {
+        outer:
+        for (int i = 0; i <= source.length - pattern.length; i++) {
+            for (int j = 0; j < pattern.length; j++) {
+                if (source[i + j] != pattern[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
+    }
+
+    // Custom implementation of lastIndexOf for byte arrays
+    private static int lastIndexOf(byte[] source, byte[] pattern) {
+        outer:
+        for (int i = source.length - pattern.length; i >= 0; i--) {
+            for (int j = 0; j < pattern.length; j++) {
+                if (source[i + j] != pattern[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
+    }
 }
