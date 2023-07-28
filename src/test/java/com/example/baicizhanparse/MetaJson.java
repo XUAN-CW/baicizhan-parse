ca952c6a3b7a99f9fac4d7d2b4db446972cecf6c
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2023-07-28 22:03:22 +0800

    Update MetaJson.java

--- src/test/java/com/example/baicizhanparse/MetaJson.java
+++ src/test/java/com/example/baicizhanparse/MetaJson.java
@@ -15,7 +15,7 @@ public class MetaJson {
         File meta = new File("meta.json");
 
         cut(inputFile,128,811,  meta);
-        cut(inputFile,1023,2022,  new File("resource.json"));
+        cut(inputFile,939,1520,  new File("resource.json"));
     }
 
     @Test
